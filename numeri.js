db97d95cec28ae45bb9e7fc1a31a2ae3ee6ddc3d
Christian Burkert <post@cburkert.de>
2012-11-23 12:15:09 +0100

    Display thousand separators

--- numeri.js
+++ numeri.js
@@ -76,7 +76,7 @@ function getCardinalSolution( num ) {
 		} else if ( part != "" ) {
 			solution = part + " " + solution;
 		}
-		num /= 1000;
+		num = Math.floor( num / 1000 );
 		power += 1;
 	}
 
@@ -134,6 +134,20 @@ function getOrdinalSolution( num ) {
 }
 
 
+function separateThousands( num ) {
+	var ns = num.toString();
+	var len = ns.length;
+	var chunklen = len > 3 ? 3 : len;
+	var separated = ns.substr( len - chunklen, chunklen );
+
+	for ( var i = len - 3; i > 0; i -= 3 ) {
+		chunklen = i > 3 ? 3 : i;
+		separated = ns.substr( i - chunklen, chunklen ) + "." + separated;
+	}
+
+	return separated;
+}
+
 function createChallenge() {
 	number = min + (max - min)*Math.random();
 	number = Math.round(number);
@@ -141,10 +155,10 @@ function createChallenge() {
 	if ( !ordinal_enabled || Math.random() < 1 - ordinalProb ) {
 		// cardinal
 		solution = getCardinalSolution( number );
-		document.getElementById("challenge").number.value = number;
+		document.getElementById("challenge").number.value = separateThousands(number);
 	} else {
 		solution = getOrdinalSolution( number );
-		document.getElementById("challenge").number.value = number + unescape("%B0");
+		document.getElementById("challenge").number.value = separateThousands(number) + unescape("%B0");
 	}
 	resetProposal();
 	var size = solution.length > 20 ? solution.length : 20;
